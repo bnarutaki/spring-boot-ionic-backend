9167f6a9e4c75547b671002215bc6e78687637d3
Bernardo Narutaki <bnarutaki@gmail.com>
2018-09-04 23:10:23 -0300

    incluindo classe categoria

--- src/main/java/com/binaru/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/binaru/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,29 @@
 package com.binaru.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.binaru.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Meu REST esta funcionando!";
+	public List<Categoria> listar() {
+		
+		Categoria cat1= new Categoria(1,"informaticaescritorio");
+		Categoria cat2= new Categoria(2,"informatica");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 		
 	}
 	
