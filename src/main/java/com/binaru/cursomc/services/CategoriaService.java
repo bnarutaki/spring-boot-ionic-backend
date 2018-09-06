a7b38898ee9cc1413fd3c71930352bbfebb267d9
Bernardo Narutaki <bnarutaki@gmail.com>
2018-09-05 22:57:20 -0300

    criando servico e repository para categoria

---
+++ src/main/java/com/binaru/cursomc/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.binaru.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.binaru.cursomc.domain.Categoria;
+import com.binaru.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> ob = repo.findById(id);
+		return ob.orElse(null);
+	}
+}
