8f51f52209434045ac22eee0ddb8744bd8c298e9
Bernardo Narutaki <bnarutaki@gmail.com>
2018-10-09 22:59:33 -0300

    end point /clientes/1

---
+++ src/main/java/com/binaru/cursomc/services/ClienteService.java
@@ -0,0 +1,26 @@
+package com.binaru.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.binaru.cursomc.domain.Categoria;
+import com.binaru.cursomc.domain.Cliente;
+import com.binaru.cursomc.repositories.ClienteRepository;
+import com.binaru.cursomc.services.exception.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	ClienteRepository repo;
+	
+
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> ob = repo.findById(id);
+		return ob.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto nao encontrao! Id = " + id + ", Tipo:" 
+				+ Cliente.class.getName()));
+	}
+}
