9a39b9a4f2f9a2de28b2bc99363e2a69da073c67
Bernardo Narutaki <bnarutaki@gmail.com>
2018-10-10 23:43:42 -0300

    atualizacap @JsonIgnore

--- src/main/java/com/binaru/cursomc/domain/Cliente.java
+++ src/main/java/com/binaru/cursomc/domain/Cliente.java
@@ -16,8 +16,7 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
 import com.binaru.cursomc.domain.enums.TipoCliente;
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 
 @Entity
@@ -33,15 +32,14 @@ public class Cliente implements Serializable{
 	private String cpfOuCnpj;
 	private Integer tipo;
 	
-	@JsonManagedReference
-	@OneToMany(mappedBy="cliente")
+		@OneToMany(mappedBy="cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
 	
 	@ElementCollection
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 	
-	@JsonBackReference
+	@JsonIgnore
 	@OneToMany(mappedBy="cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 	
