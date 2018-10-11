768eade3f6b6bd909f46a44d2928dcd63295d381
Bernardo Narutaki <bnarutaki@gmail.com>
2018-10-10 23:34:19 -0300

    endpoint pedidos/

--- src/main/java/com/binaru/cursomc/domain/Cliente.java
+++ src/main/java/com/binaru/cursomc/domain/Cliente.java
@@ -16,6 +16,7 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
 import com.binaru.cursomc.domain.enums.TipoCliente;
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 
@@ -39,7 +40,8 @@ public class Cliente implements Serializable{
 	@ElementCollection
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
-
+	
+	@JsonBackReference
 	@OneToMany(mappedBy="cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 	
---
+++ src/main/java/com/binaru/cursomc/services/PedidoService.java
@@ -0,0 +1,25 @@
+package com.binaru.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.binaru.cursomc.domain.Pedido;
+import com.binaru.cursomc.repositories.PedidoRepository;
+import com.binaru.cursomc.services.exception.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	PedidoRepository repo;
+	
+
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> ob = repo.findById(id);
+		return ob.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto nao encontrao! Id = " + id + ", Tipo:" 
+				+ Pedido.class.getName()));
+	}
+}
