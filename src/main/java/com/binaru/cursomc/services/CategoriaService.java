0dbd7b2d91a28c4355598f49193f37209f098090
Bernardo Narutaki <bnarutaki@gmail.com>
2018-11-07 09:29:20 -0200

    delete categoria

--- src/main/java/com/binaru/cursomc/services/CategoriaService.java
+++ src/main/java/com/binaru/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.binaru.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.binaru.cursomc.domain.Categoria;
 import com.binaru.cursomc.repositories.CategoriaRepository;
+import com.binaru.cursomc.services.exception.DataIntegrityException;
 import com.binaru.cursomc.services.exception.ObjectNotFoundException;
 
 @Service
@@ -31,5 +33,15 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId()); // chama o find somente para confirmar se o Id existe.
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id); // chama o find somente para confirmar se o Id existe.
+		try {
+			repo.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Nao e possível excluir uma categoria associada a produtos");
+		}
 	}
 }
